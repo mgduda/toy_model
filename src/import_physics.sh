9a3ae8e0f039dfd191cc756e6e8b1b9d59872c1b
Michael Duda <duda@ucar.edu>
2022-05-10 15:32:13 +0000

    Update import_physics.sh script to handle several special cases
    The import_physics.sh script now handles the following situations:
     - the physics directory already exists but has a different version
       of schemes and is either clean or dirty
     - the requested physics version cannot be obtained

--- src/import_physics.sh
+++ src/import_physics.sh
@@ -13,26 +13,68 @@ physics_repo=git@github.com:mgduda/shared_physics.git
 physics_dir=phys
 
 
+
 ##### Generally no need to modify script beyond this point #####
 
 if [ ! -d ${physics_dir} ]; then
     git clone ${physics_repo} ${physics_dir}
     if [ ! -d ${physics_dir} -o $? -ne 0 ]; then
-        printf "\nFailed to clone ${physics_repo} into ${physics_dir}!\n\n"
+        printf "***********************************************************\n"
+        printf "Error: Failed to clone ${physics_repo} into ${physics_dir}!\n"
+        printf "***********************************************************\n"
         exit 1
     fi
 
     cd ${physics_dir}
     git checkout ${physics_version}
     if [ $? -ne 0 ]; then
-        printf "\nFailed to checkout physics version ${physics_version}!\n\n"
+        printf "***********************************************************\n"
+        printf "Error: Failed to checkout physics version ${physics_version}!\n"
+        printf "***********************************************************\n"
 	cd ..
 	rm -rf ${physics_dir}
         exit 1
     fi
 else
-    printf "Physics directory already exists!\n\n"
+    printf "Physics directory ${physics_dir} already exists!\n"
+
+    cd ${physics_dir}
+    vers=`git describe`
+    versdirty=`git describe --dirty`
+
+    if [ "${vers}" != "${physics_version}" ]; then
+        printf "Need to switch from physics version ${vers} to ${physics_version}...\n"
+
+        if [ "${vers}" != "${versdirty}" ]; then
+            printf "***********************************************************\n"
+            printf "Error: Cannot obtain physics version ${physics_version}.\n"
+            printf "       Physics version ${vers} is currently checked out and\n"
+            printf "       has local modifications.\n"
+            printf "***********************************************************\n"
+            exit 1
+        fi
+
+        git checkout ${physics_version}
+        if [ $? -ne 0 ]; then
+            printf "***********************************************************\n"
+            printf "Error: Failed to checkout physics version ${physics_version}!\n"
+            printf "***********************************************************\n"
+            exit 1
+        fi
+
+        printf "\nPhysics are now at version ${physics_version}\n\n"
+        exit 0
+
+    elif [ "${vers}" != "${versdirty}" ]; then
+        printf "***********************************************************\n"
+        printf "NOTE: physics contain local modifications to version ${vers}\n"
+        printf "***********************************************************\n"
+        exit 0
+    fi
+
+    printf "\nPhysics are already at version ${physics_version}\n"
     exit 0
 fi
 
-printf "\nSuccessfully obtained physics version ${physics_version}!\n\n"
+printf "\nSuccessfully obtained physics version ${physics_version}!\n"
+exit 0
